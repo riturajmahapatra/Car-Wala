7d94e74b3ee63eb8a4c04439e69a864b061b6d6b
Rituraj Mahapatra <riturajmahapatra@gmail.com>
2023-10-16 22:19:32 +0530

    searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,54 +1,64 @@
 'use client';
-import React from 'react';
-import { SearchManufacturer } from '.';
-import { useState } from 'react';
+
 import Image from 'next/image';
-const SearchBtn = ({ otherClasses }: { otherClasses: string }) => (
-	<button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
-		<Image
-			src={`/magnifying-glass.svg`}
-			alt="magnifying-glass"
-			height={40}
-			width={40}
-			className="object-contain"
-		/>
-	</button>
+import React, { useState } from 'react';
+
+import SearchManufacturer from './SearchManufacturer';
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={'/magnifying-glass.svg'}
+      alt={'magnifying glass'}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
 );
 
 const SearchBar = () => {
-	const [manufacturer, setmanufacturer] = useState('');
-	const [model, setmodel] = useState(' ');
-	const handleSearch = () => {};
+  const [manufacturer, setManuFacturer] = useState('');
+  const [model, setModel] = useState('');
 
-	return (
-		<form className="searchbar" onSubmit={handleSearch}>
-			<div className="searchbar__item">
-				<SearchManufacturer
-					manufacturer={''}
-					setManuFacturer={setmanufacturer}
-				/>
-			</div>
-			<div className="searchbar__item">
-				<Image
-					src={`/model-icon.png`}
-					alt="model"
-					width={25}
-					height={25}
-					className="absolute w-[20px] h-[20px] ml-4"
-				/>
-				<input
-					type="text"
-					name="model"
-					value={model}
-					onChange={(e) => setmodel(e.target.value)}
-					placeholder="Tiguan"
-					className="searchbar__input"
-				/>
-				<SearchBtn otherClasses="sm:hidden" />
-			</div>
-			<SearchBtn otherClasses="max-sm:hidden" />
-		</form>
-	);
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === '' && model === '') {
+      return alert('Please provide some input');
+    }
+  };
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
+  );
 };
 
 export default SearchBar;
