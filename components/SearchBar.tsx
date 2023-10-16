34b8f21aec7355e9f67262c61acd934672c80289
Rituraj Mahapatra <riturajmahapatra@gmail.com>
2023-10-16 23:57:37 +0530

    CMT

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 import SearchManufacturer from './SearchManufacturer';
 
@@ -21,12 +22,42 @@ const SearchBar = () => {
   const [manufacturer, setManuFacturer] = useState('');
   const [model, setModel] = useState('');
 
+  const router = useRouter();
+
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (manufacturer === '' && model === '') {
+    if (manufacturer.trim() === '' && model.trim() === '') {
       return alert('Please provide some input');
     }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    // Create a new URLSearchParams object using the current URL search parameters
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Update or delete the 'model' search parameter based on the 'model' value
+    if (model) {
+      searchParams.set('model', model);
+    } else {
+      searchParams.delete('model');
+    }
+
+    // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    } else {
+      searchParams.delete('manufacturer');
+    }
+
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
   };
 
   return (
