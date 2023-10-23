12881d7f605e56dd72ff24cc1d064332c178a2a2
riturajmahapatra <riturajmahapatra@gmail.com>
2023-10-23 12:05:42 +0530

    complete & add later features

--- utils/index.ts
+++ utils/index.ts
@@ -46,3 +46,13 @@ export const generateCarImageUrl = (car: Carprops, angle?: string) => {
   url.searchParams.append("modelYear", `${year}`);
   return `${url}`;
 };
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
